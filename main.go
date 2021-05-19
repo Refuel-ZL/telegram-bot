212cad9450a55852125fe872e900ff7a3907fa19
strive <strive@petalmail.com>
2021-05-19 17:53:11 +0800

    feat:主动/定时发送消息

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/big"
+	rand_ "math/rand"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/robfig/cron/v3"
 	"github.com/valyala/fasthttp"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
@@ -16,30 +19,46 @@ import (
 var domain = "https://imgs.zhxiao1124.design"
 var pics []string
 
+const ChatID = "-380625611"
+
+var c = cron.New(cron.WithSeconds())
+
+func init() {
+	rand_.Seed(time.Now().UnixNano())
+}
+
 func GetPics() {
 	statusCode, body, err := fasthttp.Get(nil, domain+"/get")
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Println(err.Error())
+		return
 	}
 	if statusCode == http.StatusOK {
 		if err := json.Unmarshal(body, &pics); err != nil {
-			log.Fatalln(err.Error())
+			log.Println(err.Error())
+			return
 		}
 	}
+	rand_.Shuffle(len(pics), func(i, j int) {
+		pics[i], pics[j] = pics[j], pics[i]
+	})
 }
 
 func getpicuris(count int) []*tb.Photo {
 	var res []*tb.Photo
+	if len(pics) == 0 {
+		GetPics()
+	}
 	for i := 0; i < count; i++ {
-		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(pics))))
-		uri := fmt.Sprintf("http://imgs.zhxiao1124.design/static/%s", pics[n.Int64()])
+		uri := fmt.Sprintf("http://imgs.zhxiao1124.design/static/%s", pics[0])
 		res = append(res, &tb.Photo{File: tb.FromURL(uri)})
+		pics = pics[1:]
+		log.Println(uri)
 	}
 	return res
 }
 
 func main() {
-	GetPics()
 	b, err := tb.NewBot(tb.Settings{
 		// You can also set custom API URL.
 		// If field is empty it equals to "https://api.telegram.org".
@@ -79,6 +98,7 @@ func main() {
 			fmt.Println(err.Error())
 		}
 	})
+
 	b.Handle(tb.OnText, func(m *tb.Message) {
 		switch m.Text {
 		case "":
@@ -103,5 +123,38 @@ func main() {
 		// incoming inline queries
 	})
 
-	b.Start()
+	c.AddFunc("0 0 * * * ?", func() {
+		c_, err := b.ChatByID(ChatID)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		var cfg tb.Album
+		for _, v := range getpicuris(4) {
+			cfg = append(cfg, v)
+		}
+		_, err = b.SendAlbum(c_, cfg)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	})
+	c.Start()
+	go b.Start()
+	fmt.Println("程序启动")
+
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, os.Interrupt, syscall.SIGTERM)
+	<-s
+	// 收到信号退出无限循环
+	b.Stop()
+	fmt.Println("\ngraceful shuwdown")
+}
+
+type TimingTask struct {
+	Bot    *tb.Bot
+	ChatID string
+	Func   func(TimingTask)
+}
+
+func (t TimingTask) Run() {
+	t.Func(t)
 }
